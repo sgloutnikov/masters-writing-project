a2ba7e5ce400f408f4421114a1e1f41d0916688e
Stefan Gloutnikov <stefan@gloutnikov.com>
2018-03-08 23:40:25 -0800

    Messing around still

--- src/main/scala/edu/sjsu/cs298/SparkCoreNLP.scala
+++ src/main/scala/edu/sjsu/cs298/SparkCoreNLP.scala
@@ -7,16 +7,31 @@ import com.databricks.spark.corenlp.functions._
 
 object SparkCoreNLP {
 
+  val REVIEWS_LOCATION = "C:\\Users\\Admin\\Downloads\\YelpDataset11\\dataset\\review.json"
+
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.ERROR)
+
 
     val spark = SparkSession.builder()
       .appName("WritingProject")
       .master("local[*]")
       .getOrCreate()
-
     import spark.sqlContext.implicits._
 
+    val reviews = spark.read.json(REVIEWS_LOCATION)
+
+    val topReviewers = reviews.groupBy('user_id).count().sort(desc("count"))
+    val moreThanFiveReviews = topReviewers.filter('count > 5)
+    println(moreThanFiveReviews.count())
+    
+    /*
+    val sentences = reviews.limit(10).select($"review_id", posexplode(ssplit('text)).as(Seq("pos", "review_sentence")))
+    val results  = sentences.withColumn("sentiment", sentiment('review_sentence))
+    results.show(truncate = false)
+    */
+
+    /*
     val input = Seq(
       (1, "<xml>Stanford University is located in California. It is a great university.</xml>")
     ).toDF("id", "text")
@@ -27,6 +42,7 @@ object SparkCoreNLP {
       .select('sen, tokenize('sen).as('words), ner('sen).as('nerTags), sentiment('sen).as('sentiment))
 
     output.show(truncate = false)
+    */
 
   }
 
