0a0d94b425c98f41008fa6fda4d8377edea00321
Stefan Gloutnikov <stefan@gloutnikov.com>
2018-03-12 19:15:07 -0700

    mac doodles

--- src/main/scala/edu/sjsu/cs298/SparkCoreNLP.scala
+++ src/main/scala/edu/sjsu/cs298/SparkCoreNLP.scala
@@ -9,32 +9,28 @@ import com.mongodb.spark.MongoSpark
 object SparkCoreNLP {
 
   //val REVIEWS_LOCATION = "C:\\Users\\Admin\\Downloads\\YelpDataset11\\dataset\\review.json"
-  val REVIEWS_LOCATION = "C:\\Users\\Admin\\Downloads\\YelpDataset11\\dataset\\review_small.json"
+  val REVIEWS_LOCATION = "/Users/sgloutnikov/Downloads/dataset/review_small.json"
 
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.ERROR)
 
-
     val spark = SparkSession.builder()
-      .appName("WritingProject")
       .master("local[*]")
-      .config("spark.mongodb.input.uri", "mongodb://localhost:27017/yelp_reviews.sentiment_results")
-      .config("spark.mongodb.output.uri", "mongodb://localhost:27017/yelp_reviews.sentiment_results")
+      //.config("spark.mongodb.input.uri", "mongodb://localhost:27017/yelp_reviews.sentiment_results")
+      //.config("spark.mongodb.output.uri", "mongodb://localhost:27017/yelp_reviews.sentiment_results")
       .getOrCreate()
     import spark.sqlContext.implicits._
 
     val reviews = spark.read.json(REVIEWS_LOCATION)
-
-    /*
-    val topReviewers = reviews.groupBy('user_id).count().sort(desc("count"))
-    val moreThanFiveReviews = topReviewers.filter('count > 5)
-    println(moreThanFiveReviews.count())
-    */
+    println("Total reviews: " + reviews.count())
 
     val t0 = System.currentTimeMillis()
-    val sentences = reviews.select($"review_id", posexplode(ssplit('text)).as(Seq("pos", "review_sentence")))
-    val results  = sentences.withColumn("sentiment", sentiment('review_sentence))
-    
+    val results = reviews.select($"review_id", posexplode(ssplit('text)).as(Seq("pos", "review_sentence")))
+      .withColumn("sentiment", sentiment('review_sentence))
+    println("Total sentences: " + results.count())
+
+    results.groupBy("sentiment").count().show()
+
     //MongoSpark.write(results)
     val t1 = System.currentTimeMillis()
     println("Total time: " + (t1 - t0)/ 1000 + " seconds.")
