de380a538392161319841824a2d4b6e00b162e28
Stefan Gloutnikov <stefan@gloutnikov.com>
2018-03-14 01:59:34 -0700

    Fixed limit bug

--- java/mastersproject/src/main/java/edu/sjsu/cs298/SentenceSentimentApp.java
+++ java/mastersproject/src/main/java/edu/sjsu/cs298/SentenceSentimentApp.java
@@ -10,21 +10,28 @@ public class SentenceSentimentApp {
         List<Thread> threadList = new ArrayList<Thread>();
         int NUM_THREADS = 8;
         int limit = 1250;
-        int skip = 130000;
+        int skip;
 
         //
 
-        // After 130,000 - 140,000
+        // After 170,000 - 180,000
         for (int i = 0; i < NUM_THREADS; i++) {
-            skip = skip + (i * limit);
+            skip = 170000 + (i * limit);
             System.out.println("Thread-"+ i + " range: " + skip + "-" + (skip+limit));
             SentenceSentimentWorker worker = new SentenceSentimentWorker(limit, skip);
             Thread thread = new Thread(worker, "Thread-" + i);
             threadList.add(thread);
         }
 
+
         for (Thread t : threadList) {
             t.start();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+
     }
 }
