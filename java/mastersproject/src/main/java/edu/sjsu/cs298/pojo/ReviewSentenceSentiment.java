3da230bf71ddb438337759e5671d0381520850bb
Stefan Gloutnikov <stefan@gloutnikov.com>
2018-03-13 17:16:49 -0700

    Workers able to run

--- java/mastersproject/src/main/java/edu/sjsu/cs298/pojo/ReviewSentenceSentiment.java
+++ java/mastersproject/src/main/java/edu/sjsu/cs298/pojo/ReviewSentenceSentiment.java
@@ -2,12 +2,14 @@ package edu.sjsu.cs298.pojo;
 
 public class ReviewSentenceSentiment {
     private String review_id;
+    private String user_id;
     private String sentence;
     private int position;
     private int sentiment;
 
-    public ReviewSentenceSentiment(String review_id, String sentence, int position, int sentiment) {
+    public ReviewSentenceSentiment(String review_id, String user_id, String sentence, int position, int sentiment) {
         this.review_id = review_id;
+        this.user_id = user_id;
         this.sentence = sentence;
         this.position = position;
         this.sentiment = sentiment;
